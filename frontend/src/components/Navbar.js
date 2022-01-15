ec04b32f6ad78147e2e8c630e85c078465010f7a
skhin <88125976+skhin@users.noreply.github.com>
2022-01-15 17:46:26 +0800

    Updated css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,13 +13,13 @@ const Navbar = (click) => {
             <h4 className="nav__sqs">SQS Bags</h4>
           </a>
         </div>
-        <ul classname="navbar__links">
+
+        <ul className="navbar__links">
           <li>
             <Link to="/cart" className="cart__link">
               <i className="fas fa-shopping-cart"></i>
               <span>
-                Cart
-                <span className="cartlogo__badge">0</span>
+                Cart <span className="cartlogo__badge">0</span>
               </span>
             </Link>
           </li>
