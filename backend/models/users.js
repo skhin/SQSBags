22040bdd530f1142635b6b32e83a9b3b6a227491
qizhen <lim.qizhen@gmail.com>
2022-01-16 16:22:54 +0800

    users schema

---
+++ backend/models/users.js
@@ -0,0 +1,17 @@
+//this one creates the collection users with the stated schema that you export back to server
+const mongoose = require("mongoose");
+//help you check for the DB input
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    phone: { type: Number, required: true },
+  },
+  { collection: "users" }
+);
+//the model is an instant of the schema
+const Users = mongoose.model("Users", UserSchema);
+
+module.exports = Users;
