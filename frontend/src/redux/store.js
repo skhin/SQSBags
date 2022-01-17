2500dd6dea8b189b6649a7fe5669db37a26b09a1
qizhen <lim.qizhen@gmail.com>
2022-01-17 18:29:45 +0800

    commented out redux middleware

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -9,34 +9,44 @@ import {
   getProductDetailsReducer,
 } from "./reducers/productReducers";
 import userReducer from "./reducers/userReducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const reducer = combineReducers({
-  cart: cartReducer,
-  getAllBags: getProductsReducer,
-  getProductDetails: getProductDetailsReducer,
-  user: userReducer,
-});
+// const reducer = combineReducers({
+//   cart: cartReducer,
+//   getAllBags: getProductsReducer,
+//   getProductDetails: getProductDetailsReducer,
+//   user: userReducer,
+// });
 
 //creates an array that has all the middleware in it
 // helps in making async request in actions
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const cartItemsInLocalStorage = localStorage.getItem("cart")
-  ? JSON.parse(localStorage.getItem("cart"))
-  : [];
+// const cartItemsInLocalStorage = localStorage.getItem("cart")
+//   ? JSON.parse(localStorage.getItem("cart"))
+//   : [];
 
-const INITIAL_STATE = {
-  cart: {
-    cartItems: cartItemsInLocalStorage,
+// const INITIAL_STATE = {
+//   cart: {
+//     cartItems: cartItemsInLocalStorage,
+//   },
+// };
+
+// const store = createStore(
+//   reducer,
+//   INITIAL_STATE,
+//   // takes in state or enhancer in redux
+//   // applymiddleware adds in to the array of the middlewares
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    getAllBags: getProductsReducer,
+    getProductDetails: getProductDetailsReducer,
+    user: userReducer,
   },
-};
-
-const store = createStore(
-  reducer,
-  INITIAL_STATE,
-  // takes in state or enhancer in redux
-  // applymiddleware adds in to the array of the middlewares
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+});
 
 export default store;
